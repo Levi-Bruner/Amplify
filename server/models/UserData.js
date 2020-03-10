4b7362102c103d814b93d9d2ccb02534430ee6f8
IsaiahWright902 <icwright902@gmail.com>
2020-03-10 13:27:28 -0600

    whoops chainging to master right now

---
+++ server/models/UserData.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const UserData = new Schema(
+  {
+    creatorEmail: { type: String, required: true },
+    goodRecommends: { type: Number, default: 0 },
+    totalRecommends: { type: Number, default: 0 },
+    username: { type: String, required: true, unique: true }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+UserData.virtual("creator", {
+  localField: "creatorEmail",
+  ref: "Profile",
+  foreignField: "email",
+  justOne: true
+});
+
+export default UserData;
